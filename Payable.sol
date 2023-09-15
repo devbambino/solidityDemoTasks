0f58ab28c5da23182cbbbcd64376d6ff3d8d8c61
Fredy Del Vecchio <fremdelve@gmail.com>
2023-09-15 14:19:20 -0600

    second commit

--- Payable.sol
+++ Payable.sol
@@ -1,7 +1,5 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
 contract Payable {
 
 	/*structs---------------------------------------------------------------------------------------------
--- Primitives.sol
+++ Primitives.sol
@@ -1,26 +1,17 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
-// Useful for debugging. Remove when deploying to a live network.
-import "hardhat/console.sol";
 contract Primitives {
 
-	/*structs---------------------------------------------------------------------------------------------
-	The contract should now contain the following: 
-	setUserDetails(string calldata name, uint256 age) this function accepts 2 arguments that represent the details 
-	of the user calling the smart contract and it saves them into a defined struct, 
-	getUserDetail() this function retrieves and returns the details saved for the user calling the contract.*/
-
-	struct User{
-		string name;
-		uint256 age;
+	/*default primitives--------------------------------------------------------------------------------------------
+	Signed integers Unsigned integers Boolean Addresses Enums Bytes*/
+	uint balance;
+	int defaultInt;
+	bytes1 a = 0xa1;
+	bool public variableB = false;
+	address public owner = 0xcE2C63f43420E38B8D40CC876bf8E7644bb73005;
+	enum Status {
+		inactive,
+		active
 	}
-	mapping(address => User) public users; 
-
-	function setUserDetails(string calldata _name, uint256 _age) public {
-		users[msg.sender] = User(_name,_age);
-	}
-	
-	function getUserDetail() public view returns(User memory _userInfo){
-		return users[msg.sender];
-	}
+	Status public status;
 }
